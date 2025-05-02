7a910ea69e8fe0dee27cdf2971b8f89c417d7d9d
Seungjin in SY <xeungjin@gmail.com>
2025-05-02 18:05:34 +0900

    역순 정렬 출력

--- src/main/java/com/example/SpringMysqlConn/service/MemoService.java
+++ src/main/java/com/example/SpringMysqlConn/service/MemoService.java
@@ -2,6 +2,7 @@ package com.example.SpringMysqlConn.service;
 
 import com.example.SpringMysqlConn.MemoRepository;
 import com.example.SpringMysqlConn.dto.Memo;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class MemoService {
     }
 
     public List<Memo> getMemo() {
-        return memoRepository.findAll();
+        //return memoRepository.findAll();
+        return memoRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
     }
 }
